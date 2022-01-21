7e3a3d9c56a457aa5602702e4cdb39d8d842e0c2
Watanabe-You <77764219+Watanabe-You@users.noreply.github.com>
2022-01-21 15:08:11 +0900

    Create visible.js

---
+++ docs/temp/visible.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+var ans = "rnon";
+var t1="";
+
+for (var i = 1; i < 40; i++) {
+ if(document.getElementById("rank"+i) != null) {
+ 
+ans = "rnon";
+t1 = document.getElementById("rank"+i+"_title").innerHTML;
+
+if (t1=="") {
+document.getElementById("rank"+i).className=ans;
+}
+ }
+}
+
+};
