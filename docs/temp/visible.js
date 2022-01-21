0bac210c43e5919ef250c6bcb25aa024407c511c
Watanabe-You <77764219+Watanabe-You@users.noreply.github.com>
2022-01-21 15:11:23 +0900

    Update visible.js

--- docs/temp/visible.js
+++ docs/temp/visible.js
@@ -6,7 +6,7 @@ for (var i = 1; i < 40; i++) {
  if(document.getElementById("rank"+i) != null) {
  
 ans = "rnon";
-t1 = document.getElementById("rank"+i+"_title").innerHTML;
+t1 = document.getElementById("rank"+i+"_rk").innerHTML;
 
 if (t1=="") {
 document.getElementById("rank"+i).className=ans;
