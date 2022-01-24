785dd10eadad04c10437684eddd0b7b1dab1051a
Watanabe-You <77764219+Watanabe-You@users.noreply.github.com>
2022-01-24 19:26:09 +0900

    Update visible.js

--- docs/temp/visible.js
+++ docs/temp/visible.js
@@ -2,7 +2,7 @@ window.onload = function() {
 var ans = "rnon";
 
 for (var i = 1; i < 40; i++) {
- if(document.getElementById("rank"+i) != null) {
+if(document.getElementById("rank"+i) != null) {
  
 ans = "rnon";
 rk1 = document.getElementById("rank"+i+"_rk").innerHTML;
@@ -11,27 +11,26 @@ document.getElementById("rank"+i).className="non";
 document.getElementById("rank"+i+"_b").className="non";
 }
 else {  
-bf1 = document.getElementById("rank"+i+"_bf").innerHTML;
+ bf1 = document.getElementById("rank"+i+"_bf").innerHTML;
 
-if (bf1=="-") {
-ans = "rnew";
-}
-else {
-
- if (Number(rk1)>Number(bf1)){
- ans = "rdown";
- }
- else if (Number(rk1)<Number(bf1)){
- ans = "rup";
-  if (Number(rk1)+5<Number(bf1)){
+ if (bf1=="-") {
   ans = "rnew";
+ }
+ else {
+
+  if (Number(rk1)>Number(bf1)){
+   ans = "rdown";
+  }
+  else if (Number(rk1)<Number(bf1)){
+   ans = "rup";
+   if (Number(rk1)+5<Number(bf1)){
+    ans = "rnew";
+   }
   }
  }
-}
-document.getElementById("rank"+i+"_updown").className=ans;
+ document.getElementById("rank"+i+"_updown").className=ans;
 }
 
- }
-}
+}}
 
 };
