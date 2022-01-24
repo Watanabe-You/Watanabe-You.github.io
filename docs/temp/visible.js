b4ee41ad722e6d5dc6f95b44ae95b99ba1be0fef
Watanabe-You <77764219+Watanabe-You@users.noreply.github.com>
2022-01-24 19:25:14 +0900

    Update visible.js

--- docs/temp/visible.js
+++ docs/temp/visible.js
@@ -1,17 +1,36 @@
 window.onload = function() {
 var ans = "rnon";
-var t1="";
 
 for (var i = 1; i < 40; i++) {
  if(document.getElementById("rank"+i) != null) {
  
-ans = "non";
-t1 = document.getElementById("rank"+i+"_rk").innerHTML;
-
-if (t1=="") {
-document.getElementById("rank"+i).className=ans;
-document.getElementById("rank"+i+"_b").className=ans;
+ans = "rnon";
+rk1 = document.getElementById("rank"+i+"_rk").innerHTML;
+if (rk1=="") {
+document.getElementById("rank"+i).className="non";
+document.getElementById("rank"+i+"_b").className="non";
 }
+else {  
+bf1 = document.getElementById("rank"+i+"_bf").innerHTML;
+
+if (bf1=="-") {
+ans = "rnew";
+}
+else {
+
+ if (Number(rk1)>Number(bf1)){
+ ans = "rdown";
+ }
+ else if (Number(rk1)<Number(bf1)){
+ ans = "rup";
+  if (Number(rk1)+5<Number(bf1)){
+  ans = "rnew";
+  }
+ }
+}
+document.getElementById("rank"+i+"_updown").className=ans;
+}
+
  }
 }
 
